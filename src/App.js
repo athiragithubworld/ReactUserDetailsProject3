dbd4a185bcd395e9ab62017eeb1173cf91f9f2b2
athiragithubworld <athirahrms@gmail.com>
2023-03-06 11:55:16 +0530

    App.js file (Adding college input)

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import UserList from './Components/User/UserList';
 function App() {
 const [userList , setUserList] = useState([]);
 
-const addUserHandler = (uName ,uAge) =>{
+const addUserHandler = (uName ,uAge ,uCollege) =>{
   setUserList((prevUserList) =>{
-    return [...prevUserList ,{name:uName ,age:uAge ,id:Math.random().toString()}]
+    return [...prevUserList ,{name:uName ,age:uAge ,college:uCollege,id:Math.random().toString()}]
   })
 }
 
